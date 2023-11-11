f4a8c43ca3b6fa1406c9499d0cd303add7073b17
gwlee <leemarshal@naver.com>
2023-11-12 00:26:05 +0900

    feat: find friend by email

--- lib/services/friend_service.dart
+++ lib/services/friend_service.dart
@@ -1,9 +1,12 @@
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:tuple/tuple.dart';
 
 import '../models/friend.dart';
+import './login_service.dart';
 
 // Hive 초기화 및 어댑터 등록
 void setupHive() async {
@@ -57,6 +60,7 @@ Future<List<Friend>> getAllFriends() async {
   return friends;
 }
 
+//친구 중에서 이름으로 검색하는 기능
 Future<List<Friend>> searchAndRetrieveFriends(String startsWith, String token) async {
   // 서버로부터 ID 목록을 가져옵니다.
   final String url = 'http://10.0.2.2:8080/members/friends/searchId/$startsWith';
@@ -86,5 +90,39 @@ Future<List<Friend>> searchAndRetrieveFriends(String startsWith, String token) a
   } else {
     // 에러가 발생했을 때
     throw Exception('Failed to load friend ids');
+  }
+}
+
+//아래 메서드는 친구를 추가하기 위해 아직 친국가 아닌 회원을 찾는 기능
+Future<Tuple2<Friend, String>> findFriendByEmail(String email, String token) async {
+  final String url = 'http:://10.0.2.2:8080/members/search/email/$email';
+  final response = await http.get(
+    Uri.parse(url),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token'
+    },
+  );
+
+  if(response.statusCode == 200) {
+    var friendData = json.decode(response.body);
+    int memberId = friendData['memberId'] ?? 0;
+    String memberName = friendData['memberName'] ?? 'Unknown';
+    String profileURL = friendData['profileURL'] ?? 'default.png';
+    String friendStatus = friendData['friendStatus'];
+
+    Friend tempFriend = Friend.fromJson({
+      'friendId': memberId,
+      'specifiedName': memberName,
+      'birthDate': '1900-01-01', // 임의로 채운 값
+      'profileURL': profileURL,
+      'myGive': false, // 임의로 채운 값
+      'myTake': false, // 임의로 채운 값
+    });
+
+    return Tuple2<Friend, String>(tempFriend, friendStatus);
+  } else {
+    throw Exception;
   }
 }
