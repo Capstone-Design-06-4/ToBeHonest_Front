0b50c6a033831c95b33865c254b30aab25484eba
gwlee <leemarshal@naver.com>
2023-11-16 03:29:20 +0900

    feat: fetch, save, get wishItem

--- lib/services/wishlist_service.dart
+++ lib/services/wishlist_service.dart
@@ -7,10 +7,55 @@ import 'package:tuple/tuple.dart';
 
 import '../models/friend.dart';
 import '../models/item.dart';
+import '../models/wishItem.dart';
+
 import './login_service.dart';
 
-Future<void> fetchWishlist(String token) async {
-  final url = Uri.parse('http://10.0.2.2:8080/wishlist/all/');
+Future<void> saveWishItemsToLocal(dynamic wishItems) async {
+  var box = await Hive.openBox<WishItem>('wishItemBox');
+
+  if(wishItems is List<WishItem>) {
+    await box.clear();
+    for (var wishItem in wishItems) {
+      await box.put(wishItem.wishItemId, wishItem);
+    }
+  } else if (wishItems is WishItem) {
+      await box.put(wishItems.wishItemId, wishItems);
+    } else {
+    throw ArgumentError('The argument must be a WishItem or List<WishItem>');
+  }
+  await box.close();
+}
+
+Future<List<WishItem>> getAllWishItems() async {
+  var box = await Hive.openBox<WishItem>('wishItemBox');
+  List<WishItem> wishItems = box.values.toList();
+  await box.close();
+  return wishItems;
+}
+
+Future<void> fetchWishItems(String token) async {
+  final String memberID = await getID() ?? '0';
+  if(memberID == '0') throw Exception('다시 로그인해주세요.');
+  final url = Uri.parse('http://10.0.2.2:8080/wishlist/all/$memberID');
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer $token",
+  };
+  try {
+    final response = await http.post(url, headers: headers);
+    if(response.statusCode == 200) {
+      List<dynamic> wishItemJsonList = json.decode(response.body);
+      List<WishItem> wishItemList =
+        wishItemJsonList.map((jsonItem) => WishItem.fromJson(jsonItem)).toList();
+      await saveWishItemsToLocal(wishItemList);
+      print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
+    } else {
+      print('위시 아이템 가져오기 실패: ${response.statusCode}');
+    }
+  } catch(e) {
+    print('오류 발생: $e');
+  }
 }
 
 Future<void> addWishlist(String itemID, String token) async {
