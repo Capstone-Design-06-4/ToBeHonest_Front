2e78e0bafdd2340d56a795d4056432d02bb4c437
gwlee <leemarshal@naver.com>
2023-11-17 03:35:39 +0900

    feat: [fetch, get] [all, progress, completed, used] wishItems

--- lib/services/wishlist_service.dart
+++ lib/services/wishlist_service.dart
@@ -34,6 +34,75 @@ Future<List<WishItem>> getAllWishItems() async {
   return wishItems;
 }
 
+Future<void> saveProgressItemsToLocal(dynamic wishItems) async {
+  var box = await Hive.openBox<WishItem>('progressWishItemBox');
+
+  if(wishItems is List<WishItem>) {
+    await box.clear();
+    for (var wishItem in wishItems) {
+      await box.put(wishItem.wishItemId, wishItem);
+    }
+  } else if (wishItems is WishItem) {
+    await box.put(wishItems.wishItemId, wishItems);
+  } else {
+    throw ArgumentError('The argument must be a WishItem or List<WishItem>');
+  }
+  await box.close();
+}
+
+Future<List<WishItem>> getProgressWishItems() async {
+  var box = await Hive.openBox<WishItem>('progressWishItemBox');
+  List<WishItem> wishItems = box.values.toList();
+  await box.close();
+  return wishItems;
+}
+
+Future<void> saveCompletedItemsToLocal(dynamic wishItems) async {
+  var box = await Hive.openBox<WishItem>('completedWishItemBox');
+
+  if(wishItems is List<WishItem>) {
+    await box.clear();
+    for (var wishItem in wishItems) {
+      await box.put(wishItem.wishItemId, wishItem);
+    }
+  } else if (wishItems is WishItem) {
+    await box.put(wishItems.wishItemId, wishItems);
+  } else {
+    throw ArgumentError('The argument must be a WishItem or List<WishItem>');
+  }
+  await box.close();
+}
+
+Future<List<WishItem>> getCompletedWishItems() async {
+  var box = await Hive.openBox<WishItem>('completedWishItemBox');
+  List<WishItem> wishItems = box.values.toList();
+  await box.close();
+  return wishItems;
+}
+
+Future<void> saveUsedItemsToLocal(dynamic wishItems) async {
+  var box = await Hive.openBox<WishItem>('usedWishItemBox');
+
+  if(wishItems is List<WishItem>) {
+    await box.clear();
+    for (var wishItem in wishItems) {
+      await box.put(wishItem.wishItemId, wishItem);
+    }
+  } else if (wishItems is WishItem) {
+    await box.put(wishItems.wishItemId, wishItems);
+  } else {
+    throw ArgumentError('The argument must be a WishItem or List<WishItem>');
+  }
+  await box.close();
+}
+
+Future<List<WishItem>> getUsedWishItems() async {
+  var box = await Hive.openBox<WishItem>('usedWishItemBox');
+  List<WishItem> wishItems = box.values.toList();
+  await box.close();
+  return wishItems;
+}
+
 Future<void> fetchWishItems(String token) async {
   final String memberID = await getID() ?? '0';
   if(memberID == '0') throw Exception('다시 로그인해주세요.');
@@ -58,6 +127,78 @@ Future<void> fetchWishItems(String token) async {
   }
 }
 
+Future<void> fetchProgressWishItems(String token) async {
+  final String memberID = await getID() ?? '0';
+  if(memberID == '0') throw Exception('다시 로그인해주세요.');
+  final url = Uri.parse('http://10.0.2.2:8080/wishlist/progress/$memberID');
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer $token",
+  };
+  try {
+    final response = await http.post(url, headers: headers);
+    if(response.statusCode == 200) {
+      List<dynamic> wishItemJsonList = json.decode(response.body);
+      List<WishItem> wishItemList =
+      wishItemJsonList.map((jsonItem) => WishItem.fromJson(jsonItem)).toList();
+      await saveProgressItemsToLocal(wishItemList);
+      print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
+    } else {
+      print('위시 아이템 가져오기 실패: ${response.statusCode}');
+    }
+  } catch(e) {
+    print('오류 발생: $e');
+  }
+}
+
+Future<void> fetchCompletedWishItems(String token) async {
+  final String memberID = await getID() ?? '0';
+  if(memberID == '0') throw Exception('다시 로그인해주세요.');
+  final url = Uri.parse('http://10.0.2.2:8080/wishlist/completed/$memberID');
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer $token",
+  };
+  try {
+    final response = await http.post(url, headers: headers);
+    if(response.statusCode == 200) {
+      List<dynamic> wishItemJsonList = json.decode(response.body);
+      List<WishItem> wishItemList =
+      wishItemJsonList.map((jsonItem) => WishItem.fromJson(jsonItem)).toList();
+      await saveCompletedItemsToLocal(wishItemList);
+      print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
+    } else {
+      print('위시 아이템 가져오기 실패: ${response.statusCode}');
+    }
+  } catch(e) {
+    print('오류 발생: $e');
+  }
+}
+
+Future<void> fetchUsedWishItems(String token) async {
+  final String memberID = await getID() ?? '0';
+  if(memberID == '0') throw Exception('다시 로그인해주세요.');
+  final url = Uri.parse('http://10.0.2.2:8080/wishlist/used/$memberID');
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer $token",
+  };
+  try {
+    final response = await http.post(url, headers: headers);
+    if(response.statusCode == 200) {
+      List<dynamic> wishItemJsonList = json.decode(response.body);
+      List<WishItem> wishItemList =
+      wishItemJsonList.map((jsonItem) => WishItem.fromJson(jsonItem)).toList();
+      await saveUsedItemsToLocal(wishItemList);
+      print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
+    } else {
+      print('위시 아이템 가져오기 실패: ${response.statusCode}');
+    }
+  } catch(e) {
+    print('오류 발생: $e');
+  }
+}
+
 Future<void> addWishlist(String itemID, String token) async {
   final url = Uri.parse('http://10.0.2.2:8080/wishlist/add/$itemID');
   final headers = {
@@ -65,7 +206,7 @@ Future<void> addWishlist(String itemID, String token) async {
     'Authorization': "Bearer $token",
   };
   try {
-    final response = await http.get(url, headers: headers);
+    final response = await http.post(url, headers: headers);
     if(response.statusCode == 200) {
       print('위시리스트에 정상적으로 등록되었습니다.');
     } else {
