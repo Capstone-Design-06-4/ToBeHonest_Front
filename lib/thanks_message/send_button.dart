ec7069fb5cec790f7a79edbb8f7717669b2387b3
gwlee <leemarshal@naver.com>
2023-11-22 04:35:30 +0900

    feat: thanks_message send widget

---
+++ lib/thanks_message/send_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:get/get.dart';
+import './ProductDetailsWidget.dart';
+import '../controllers/image_picker_controller.dart';
+import '../models/wishItem.dart';
+import '../models/message.dart';
+import '../services/message_service.dart';
+import '../services/login_service.dart';
+
+class SendButton extends StatelessWidget {
+  final ImagePickerController controller;
+  final String titleContent; // TextFormField의 내용
+  final String textFieldContent; // TextFormField의 내용
+  final WishItem wishItem; // WishItem 인스턴스
+  late Message message;
+  late String? token;
+  SendButton({
+    Key? key,
+    required this.controller,
+    required this.titleContent, // 생성자에 추가
+    required this.textFieldContent, // 생성자에 추가
+    required this.wishItem, // 생성자에 추가
+  }) : super(key: key);
+
+  Future<void> createMessage() async {
+    token = await getToken();
+    String? myID = await getID();
+    if(token == null || myID == null) throw Exception('로그인 다시하세요.');
+    message = Message(wishItemId: wishItem.wishItemId,
+        senderId: int.parse(myID),
+        receiverId: 0,
+        title: titleContent,
+        contents: textFieldContent,
+        messageType: MessageType.THANKS_MSG,
+        fundMoney: wishItem.fundAmount);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        if (controller.selectedImage.value != null) {
+          File selectedFile = File(controller.selectedImage.value!.path);
+          await sendThanksMessage(message, selectedFile, token!);
+        } else {
+          // 이미지가 선택되지 않았을 경우 처리
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('사진을 선택해주세요.')),
+          );
+        }
+      },
+      child: Text('전송하기'),
+    );
+  }
+}
--- lib/thanks_message/thanks_message_view.dart
+++ lib/thanks_message/thanks_message_view.dart
@@ -3,6 +3,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:get/get.dart';
 import './ProductDetailsWidget.dart';
+import './send_button.dart';
 import '../controllers/image_picker_controller.dart';
 import '../models/wishItem.dart';
 import '../models/message.dart';
@@ -11,6 +12,8 @@ class ThanksMessage extends StatelessWidget {
   final WishItem wishItem; // WishItem 인스턴스를 필수 파라미터로 추가
   final ImagePicker _picker = ImagePicker();
   final controller = Get.put(ImagePickerController()); // 컨트롤러 인스턴스화
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController textEditingController = TextEditingController();
 
   ThanksMessage({Key? key, required this.wishItem})
       : super(key: key); // 생성자에 required 파라미터 추가
@@ -40,8 +43,12 @@ class ThanksMessage extends StatelessWidget {
             flex: 2,
             child: Padding(
               padding: EdgeInsets.all(8.0),
-              child: Text(
-                '제목',
+              child: TextFormField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  labelText: '제목',
+                  border: OutlineInputBorder(),
+                ),
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
@@ -61,6 +68,7 @@ class ThanksMessage extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 8.0),
               child: TextFormField(
+                controller: textEditingController,
                 decoration: InputDecoration(
                   labelText: '내용',
                   border: OutlineInputBorder(),
@@ -73,21 +81,11 @@ class ThanksMessage extends StatelessWidget {
             flex: 2,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // 전송 로직 구현
-                  if (controller.selectedImage.value != null) {
-                    // 이미지 파일 객체 생성
-                    File selectedFile = File(controller.selectedImage.value!.path);
-
-                    // 이미지 처리 함수 호출
-                    //await sendMessage(selectedFile);
-                  } else {
-                    // 이미지가 선택되지 않았을 경우의 처리
-                    // 예: 사용자에게 알림 표시
-                  }
-                },
-                child: Text('전송하기'),
+              child: SendButton(
+                controller: controller,
+                titleContent: titleController.text,
+                textFieldContent: textEditingController.text,
+                wishItem: this.wishItem,
               ),
             ),
           ),
