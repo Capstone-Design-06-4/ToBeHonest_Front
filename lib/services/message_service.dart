f3f528a420841d7bf4044363b3ca1ac01de64b6c
gwlee <leemarshal@naver.com>
2023-11-23 02:23:54 +0900

    feat: celebrateMessage

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io' as io;
 import 'package:path/path.dart';
@@ -10,18 +11,12 @@ Future<http.Response> sendThanksMessage(Message message, io.File selectedImage, 
   try {
     var request = http.MultipartRequest('POST', uri)
       ..headers['Authorization'] = "Bearer $token"
-      ..fields['wishItemId'] = message.wishItemId.toString()
-      ..fields['senderId'] = message.senderId.toString()
-      ..fields['receiverId'] = message.receiverId.toString()
-      ..fields['title'] = message.title
-      ..fields['contents'] = message.contents
-      ..fields['messageType'] = message.messageType.toString().split('.').last
-      ..fields['fundMoney'] = message.fundMoney.toString();
+      ..fields['request'] = json.encode(message.toJson()); // JSON 데이터를 인코딩하여 추가
 
     var stream = http.ByteStream(selectedImage.openRead());
     var length = await selectedImage.length();
     var multipartFile = http.MultipartFile(
-      'images', // Field name for files (as expected by server)
+      'images', // 서버에서 기대하는 필드 이름
       stream,
       length,
       filename: basename(selectedImage.path),
@@ -33,16 +28,38 @@ Future<http.Response> sendThanksMessage(Message message, io.File selectedImage, 
     var response = await http.Response.fromStream(streamedResponse);
 
     if (response.statusCode == 200) {
-      // Handle successful response
+      // 성공적인 응답 처리
       return response;
     } else {
-      // Handle non-200 responses
-      // You can throw an exception or handle it in any other way you prefer
+      // 200이 아닌 응답 처리
       throw Exception('Failed to send message. Status code: ${response.statusCode}');
     }
   } catch (e) {
-    // Handle any exceptions that occur during the request
+    // 요청 중 발생하는 예외 처리
     print('Error sending message: $e');
-    rethrow; // or you can return a custom response or handle the error differently
+    rethrow; // 다시 예외 발생시키기
+  }
+}
+
+Future<void> sendCelebrateMessage(Message message, String token) async {
+  final url = Uri.parse('http://10.0.2.2:8080/message/send-celebrate');
+
+  try {
+    // MultipartRequest 생성
+    var request = http.MultipartRequest('POST', url)
+      ..headers['Authorization'] = "Bearer $token"
+      ..fields['request'] = json.encode(message.toJson());
+
+    // 요청 보내기
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200) {
+      print('메시지가 정상적으로 전송되었습니다.');
+    } else {
+      print('메시지 전송에 실패했습니다. 상태 코드: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('오류 발생: $e');
   }
 }
