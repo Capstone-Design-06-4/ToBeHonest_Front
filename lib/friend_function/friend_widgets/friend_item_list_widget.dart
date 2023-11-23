e1e2ff99e070a748d49381f67967346ce9e775e0
gwlee <leemarshal@naver.com>
2023-11-24 01:37:40 +0900

    feat: friend_item_list_widget

---
+++ lib/friend_function/friend_widgets/friend_item_list_widget.dart
@@ -0,0 +1,107 @@
+//item_list.dart
+
+import 'package:flutter/material.dart';
+import 'package:tobehonest/friend_function/friend_view/friend_item_detailed_view.dart';
+import 'package:tobehonest/models/wishItem.dart';
+
+class WishItemList extends StatelessWidget {
+  final List<WishItem> wishItems;
+  final String searchText;
+
+  WishItemList({required this.wishItems, required this.searchText});
+
+  @override
+  Widget build(BuildContext context) {
+    List<WishItem> filteredWishItems = _filterWishItems();
+
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.8,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: filteredWishItems.length,
+      itemBuilder: (ctx, index) => InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ItemDetailed(wishItem: filteredWishItems[index]),
+            ),
+          );
+        },
+        child: Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.grey.shade300, width: 1),
+                  ),
+                  padding: EdgeInsets.all(8.0),
+                  child: Image.network(
+                    filteredWishItems[index].image,
+                    fit: BoxFit.contain,
+                    width: 110,
+                    height: 110,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '${filteredWishItems[index].itemName}',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+                SizedBox(height: 10),
+                Stack(
+                  children: [
+                    Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(color: Colors.grey.shade300, width: 1),
+                      ),
+                    ),
+                    Opacity(
+                      opacity: 0.6,
+                      child: Container(
+                        height: 20,
+                        width: (MediaQuery.of(ctx).size.width - 245) *
+                            (filteredWishItems[index].fundAmount / filteredWishItems[index].itemPrice),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        '${(filteredWishItems[index].fundAmount / filteredWishItems[index].itemPrice * 100).toStringAsFixed(2)}%',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<WishItem> _filterWishItems() {
+    return wishItems
+        .where((item) =>
+        item.itemName.toLowerCase().contains(searchText.toLowerCase()))
+        .toList();
+  }
+}
