292a7f7bcf8a1310b69fabc16aaad549bfbcaaf3
gwlee <leemarshal@naver.com>
2023-11-24 01:25:27 +0900

    fix: handling int? friendID null

--- lib/controllers/friend_wishlist_controller.dart
+++ lib/controllers/friend_wishlist_controller.dart
@@ -19,7 +19,7 @@ class friendWishListController extends GetxController {
       String? token = await getToken();
       print('토큰: $token');
       if (token == null) throw Exception("토큰이 없습니다.");
-
+      if(friendID == null) throw Exception("friendID가 없습니다.");
       await fetchFriendWishItems(friendID, token);
 
       List<WishItem> allWishItems = [];
