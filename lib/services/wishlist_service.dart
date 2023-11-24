092fd536de2d43ccaf8c0a40c1108b96532a5df3
gwlee <leemarshal@naver.com>
2023-11-24 11:39:05 +0900

    feat: filter to usedItem by messaged not messaged

--- lib/services/wishlist_service.dart
+++ lib/services/wishlist_service.dart
@@ -127,6 +127,29 @@ Future<List<WishItem>> getUsedWishItems() async {
   return wishItems;
 }
 
+Future<void> saveThankItemsToLocal(dynamic wishItems) async {
+  var box = await Hive.openBox<WishItem>('thankWishItemBox');
+
+  if(wishItems is List<WishItem>) {
+    await box.clear();
+    for (var wishItem in wishItems) {
+      await box.put(wishItem.wishItemId, wishItem);
+    }
+  } else if (wishItems is WishItem) {
+    await box.put(wishItems.wishItemId, wishItems);
+  } else {
+    throw ArgumentError('The argument must be a WishItem or List<WishItem>');
+  }
+  await box.close();
+}
+
+Future<List<WishItem>> getThankWishItems() async {
+  var box = await Hive.openBox<WishItem>('thankWishItemBox');
+  List<WishItem> wishItems = box.values.toList();
+  await box.close();
+  return wishItems;
+}
+
 Future<void> saveFriendItemsToLocal(dynamic wishItems) async {
   var box = await Hive.openBox<WishItem>('friendWishItemBox');
 
@@ -242,9 +265,39 @@ Future<void> fetchUsedWishItems(String token) async {
       // UTF-8로 디코딩한 후 JSON으로 파싱
       String decodedResponse = utf8.decode(response.bodyBytes);
       List<dynamic> wishItemJsonList = json.decode(decodedResponse);
-      List<WishItem> wishItemList =
-      wishItemJsonList.map((jsonItem) => WishItem.fromJson(jsonItem)).toList();
+      List<WishItem> wishItemList = wishItemJsonList
+          .where((jsonItem) => jsonItem['isMessaged'] == 'NOT_MESSAGED') // 조건에 맞는 항목만 필터링
+          .map((jsonItem) => WishItem.fromJson(jsonItem)) // 필터링된 항목을 WishItem 객체로 변환
+          .toList(); // 결과를 리스트로 변환
       await saveUsedItemsToLocal(wishItemList);
+      print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
+    } else {
+      print('위시 아이템 가져오기 실패: ${response.statusCode}');
+    }
+  } catch(e) {
+    print('오류 발생: $e');
+  }
+}
+
+Future<void> fetchThankWishItems(String token) async {
+  final String memberID = await getID() ?? '0';
+  if(memberID == '0') throw Exception('다시 로그인해주세요.');
+  final url = Uri.parse('http://10.0.2.2:8080/wishlist/used/$memberID');
+  final headers = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Authorization': "Bearer $token",
+  };
+  try {
+    final response = await http.get(url, headers: headers);
+    if(response.statusCode == 200) {
+      // UTF-8로 디코딩한 후 JSON으로 파싱
+      String decodedResponse = utf8.decode(response.bodyBytes);
+      List<dynamic> wishItemJsonList = json.decode(decodedResponse);
+      List<WishItem> wishItemList = wishItemJsonList
+          .where((jsonItem) => jsonItem['isMessaged'] == 'MESSAGED') // 조건에 맞는 항목만 필터링
+          .map((jsonItem) => WishItem.fromJson(jsonItem)) // 필터링된 항목을 WishItem 객체로 변환
+          .toList(); // 결과를 리스트로 변환
+      await saveThankItemsToLocal(wishItemList);
       print('위시 아이템 가져오기 성공: ${wishItemList.length}개의 아이템이 있습니다.');
     } else {
       print('위시 아이템 가져오기 실패: ${response.statusCode}');
