55aa786ec01cb3cd9267acf8e25b806354ce462b
gwlee <leemarshal@naver.com>
2023-11-26 00:48:05 +0900

    feat: friend_product_controller

---
+++ lib/controllers/friend_product_controller.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+import 'package:tobehonest/models/wishItem.dart';
+import 'package:tobehonest/services/wishlist_service.dart';
+import 'package:tobehonest/services/login_service.dart';
+
+class FriendProductController extends GetxController {
+  var wishItem = WishItem(
+      wishItemId: -1,
+      itemBrand: '',
+      itemName: '',
+      image: '',
+      itemPrice: 0,
+      fundAmount: 0,
+      itemId: -1).obs; // 관찰 가능한 WishItem 객체
+  var friendID;
+
+  FriendProductController(WishItem initialWishItem, int friendID) {
+    wishItem.value = initialWishItem;
+    this.friendID = friendID;
+  }
+
+  Future<void> updateWishItem() async {
+    String? token = await getToken();
+    if(token == null) throw Exception('로그인 다시하세요.');
+    List<WishItem> list = await getFriendWishItemByWishItemID(friendID, wishItem.value.wishItemId, token);
+    wishItem.value = list[0];
+  }
+
+// 필요한 경우 추가 메서드를 구현합니다.
+}
