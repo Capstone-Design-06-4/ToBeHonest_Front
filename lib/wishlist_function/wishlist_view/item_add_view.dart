f19ab10890e8dd3fa72b5a398272bc1f551c0532
gw282 <fear369@cau.ac.kr>
2023-12-02 20:52:59 +0900

    :sparkles: Feat: wishitem add modal is completed.
    feat: wishitem add modal is completed.

--- lib/wishlist_function/wishlist_view/item_add_view.dart
+++ lib/wishlist_function/wishlist_view/item_add_view.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:tobehonest/style.dart';
 import 'package:tobehonest/models/item.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:tobehonest/controllers/wishlist_controlller.dart';
 import 'package:tobehonest/wishlist_function/wishlist_widgets/wishlist_add/item_add_search_widget.dart';
 
@@ -142,10 +143,7 @@ class _NewItemPageState extends State<NewItemPage> {
                       print('선택한 상품 가격: ${selectedItem!.price}');
 
                       // 여기에 위시리스트에 추가하는 로직을 추가하세요.
-                      await wishListController.addToWishlistWithSnackbar(selectedItem!);
-                      _resetPage(); // 페이지 초기화 호출
-                      Navigator.pop(context);
-                      Navigator.pop(context);
+                      ModalUtils.showFriendModal(context, selectedItem!);
                     }
                   }
                       : null,
@@ -174,5 +172,167 @@ class _NewItemPageState extends State<NewItemPage> {
   }
 }
 
+class ModalUtils {
+  static void showFriendModal(BuildContext context, Item selectedItem) {
+    final WishListController wishListController = Get.put(WishListController());
+
+    String formatNumber(int number) {
+      final formatter = NumberFormat('#,###');
+      return formatter.format(number);
+    }
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 300, // 모달 높이 크기
+            decoration: BoxDecoration(
+              color: Colors.white, // 모달 배경색
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 5,),
+                Container(
+                  height: 60.0,
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Center(
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                      leading: CircleAvatar(
+                        backgroundColor: AppColor.swatchColor,
+                        child: Icon(Icons.question_mark, color: Colors.white),
+                      ),
+                      title: Text(
+                        '위시리스트에 추가하시겠어요?',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          Navigator.pop(context); // 예시: 현재 페이지를 닫음
+                        },
+                      ),
+                    ),
+                  ),
+
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          child: Container(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                selectedItem.image,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 6),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Text(
+                                    selectedItem.brand.length > 8
+                                        ? '${selectedItem.brand.substring(0, 8)}...'
+                                        : selectedItem.brand,
+                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                selectedItem.name,
+                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Text(
+                                    '펀딩 총액: ',
+                                    style: TextStyle(fontSize: 18,),
+                                  ),
+                                  Text(
+                                    '${formatNumber(selectedItem.price)}',
+                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColor.textColor),
+                                  ),
+                                  Text(
+                                    ' 원',
+                                    style: TextStyle(fontSize: 18,),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      await wishListController.addToWishlistWithSnackbar(selectedItem!);
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+
+                    child: FittedBox(
+                      child: Text(
+                        '추가하기',
+                        style: TextStyle(fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        backgroundColor: Colors.transparent
+    );
+  }
+}
 
 
