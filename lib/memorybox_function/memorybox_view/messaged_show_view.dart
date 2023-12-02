58433961e15982dc40051696b1cf1b2206258e94
gw282 <fear369@cau.ac.kr>
2023-12-02 19:38:44 +0900

    :sparkles: Feat: Memory box messaged_show page UI is completed.
    feat: Memory box messaged_show page UI is completed.

--- lib/memorybox_function/memorybox_view/messaged_show_view.dart
+++ lib/memorybox_function/memorybox_view/messaged_show_view.dart
@@ -123,7 +123,89 @@ class _MessagedShowPageState extends State<MessagedShowPage> {
                 ),
               ),
             ),
-            // 이부분에 메시지 목록 띄우기
+            Container(
+              margin: EdgeInsets.only(right: 24, left: 24, bottom: 16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,  // 원하는 테두리 색상 설정
+                  width: 1.0,  // 원하는 테두리 두께 설정
+                ),
+                borderRadius: BorderRadius.circular(8.0),  // 원하는 테두리의 둥근 정도 설정
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,  // 수정된 부분
+                  children: [
+                    Icon(
+                      Icons.title,
+                      size: 30,
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '제목입니다',
+                          style: const TextStyle(fontSize: 18),
+                          textAlign: TextAlign.right,  // 텍스트를 우측 정렬로 설정
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 24, left: 24, bottom: 16),
+              width: double.infinity,  // 원하는 가로 크기 설정
+              height: 230,  // 원하는 세로 크기 설정
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,  // 원하는 테두리 색상 설정
+                  width: 1.0,  // 원하는 테두리 두께 설정
+                ),
+                borderRadius: BorderRadius.circular(8.0),  // 원하는 테두리의 둥근 정도 설정
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Text(
+                          '미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다.. '
+                              '그찐따같던 나루토가 맞나? 진짜 나루토는 전설이다..진짜옛날에 맨날나루토봘는데 왕같은존재인 호카게 되서 세계최강 전설적인 영웅이된'
+                              '나루토보면 진짜내가다 감격스럽고 나루토 노래부터 명장면까지 가슴울리는장면들이 뇌리에 스치면서 가슴이 웅장해진다.. ',
+                          style: const TextStyle(fontSize: 16),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://via.placeholder.com/200'),  // 여기에 실제 이미지의 URL을 넣어주세요.
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                // 필요한 경우 여기에 추가적인 위젯을 넣을 수 있습니다.
+              ),
+            ),
+            SizedBox(height: 25,),
             Padding(
               padding: EdgeInsets.all(8.0),
               child: Column(
@@ -140,18 +222,19 @@ class _MessagedShowPageState extends State<MessagedShowPage> {
                               Navigator.pop(context);
                               Navigator.pop(context);
                             },
-
                             style: ElevatedButton.styleFrom(
-                              primary: Colors.orange.shade300,
+                              primary: AppColor.backgroundColor,
                               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(20),
                               ),
+                              minimumSize: Size(0, 36),
                             ),
                             child: FittedBox(
                               child: Text(
                                 '홈으로 이동하기',
-                                style: TextStyle(fontSize: 16),
+                                style: TextStyle(fontSize: 18,
+                                ),
                               ),
                             ),
                           ),
@@ -159,6 +242,7 @@ class _MessagedShowPageState extends State<MessagedShowPage> {
                       ],
                     ),
                   ),
+                  SizedBox(height: 15,)
                 ],
               ),
             ),
